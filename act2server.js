e511d45324fec5f7ea34568bc53b36392eca2b6f
Soraia <Soraia@So-MacBook-Pro.local>
2018-02-17 11:48:08 -0500

    fixed sintax

--- act2server.js
+++ act2server.js
@@ -33,6 +33,7 @@ server.listen(PORTONE, function() {
 
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORTONE);
+});
 
   server.listen(PORTTWO, function() {
 
