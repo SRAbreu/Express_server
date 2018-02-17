d402c1ffbbb9677df1819cef17ce58eaa322c501
Soraia <Soraia@So-MacBook-Pro.local>
2018-02-17 11:59:46 -0500

    new fix

--- act2server.js
+++ act2server.js
@@ -20,20 +20,21 @@ function handleRequestTwo(request, response) { //whats the request? req to the u
   // Send the below string to the client when the user visits the PORT URL
   response.end("I guess javasccript is not your forte! " + request.url);
 } //creates the strung & sends it back to the browser.
-//this sends back to the html.
-
+//this sends back to the html/browser.
+=====================================
 
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var serverOne = http.createServer(handleRequest);
-var serverTwo = http.createServer(handleRequest);
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
 
 // Start our server so that it can begin listening to client requests.
+//URL request from the browser
 server.listen(PORTONE, function() {
 
-  // Log (server-side) when our server has started
+  // Log (server-side) when our server has started. //its going to log to the terminal
   console.log("Server listening on: http://localhost:" + PORTONE);
-});
+});//when you use server &
 
   server.listen(PORTTWO, function() {
 
